e3ec674b6d3aaf04d9ff958b76338abf982609dc
yogita <yogita.paghdar@bistasolutions.com>
2017-01-16 18:51:16 +0530

    import sale orders refs #964

--- odooconnector_sale/models/odooconnector.py
+++ odooconnector_sale/models/odooconnector.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # © 2015 Malte Jacobi (maljac @ github)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import datetime
 from openerp import models, fields,api
+from openerp.tools.safe_eval import safe_eval
+
 from openerp.addons.connector.session import ConnectorSession
 from openerp.addons.odooconnector_base.unit.import_synchronizer import import_batch
 
@@ -15,7 +18,6 @@ class OdooBackend(models.Model):
         string='Default SO export backend',
         help='Use this backend as default for the SO process.'
     )
-    
     default_export_so_domain = fields.Char(
         string='Export Sale Order Domain',
         default='[]'
@@ -23,6 +25,9 @@ class OdooBackend(models.Model):
     default_import_so_domain = fields.Char(
         string='Import Sale Order Domain',
         default='[]'
+    )
+    import_so_since=fields.Datetime(
+        string='Import SO Since'
     )
 
     @api.model
@@ -32,11 +37,13 @@ class OdooBackend(models.Model):
                                    context=self.env.context)
         backends=self.search([('version','=','1000')])
         for backend in backends:
-            filters = self.default_import_so_domain
-            if filters and isinstance(filters, str):
-                filters = eval(filters)
+            filters = backend.default_import_so_domain
+            if filters and isinstance(filters, (str,unicode)):
+                filters = safe_eval(filters)
+            domain=[('write_date','>=',backend.import_so_since)]
             import_batch(session, 'odooconnector.sale.order', backend.id,
-                         filters)
+                         filters+domain)
+            backend.write({'import_so_since':datetime.datetime.now()})
 
         return True
     
